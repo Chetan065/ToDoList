3ffa281dbf5bce417bd93aa1a2b6f0be41636b3a
Chetan <agarwalchaitan1065@gmail.com>
2023-08-15 13:27:51 +0530

    Add all files

--- HTML/todolist1.js
+++ HTML/todolist1.js
@@ -4,6 +4,6 @@ const addButtonEl = document.getElementById("add-button")
 const todolistEl = document.getElementById("todolist")
 addButtonEl.addEventListener("click", function () {
     let inputvalue = inputfielsEl.value;
+    if(inputvalue != "") {todolistEl.innerHTML += `<li class=" p-2  rounded-md text-2xl ">${inputvalue}</li>`}
     inputfielsEl.value = "";
-    todolistEl.innerHTML += `<li class=" p-2  rounded-md text-2xl ">${inputvalue}</li>`
 })
